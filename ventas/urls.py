89061f6a59bdee2817d60f05d3f1b4c0340b1130
Andrew <andrewrfofficial@gmail.com>
2023-05-06 13:55:19 -0400

    beta v-0.1.17

--- ventas/urls.py
+++ ventas/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('caja', views.Caja, name="caja"),
     path('reportes', views.Reportes, name="reportes"),
     path('clientes', views.Clientes, name="clientes"),
-    path('clientes/nuevocliente', views.EditCliente, name="editcliente"),
+    path('clientes/nuevocliente', views.NuevoCliente, name="nuevocliente"),
     path('clientes/editcliente/<int:id_cliente>', views.EditCliente, name="editcliente"),
+    path('clientes/eliminarcliente/<int:id_cliente>', views.ElimnarCliente, name="eliminarcliente"),
     path('getproducto/<int:id_producto>', views.get_Productos, name="get_Productos"),
     path('imprimirfactura', views.imprimir_pdf , name="imprimir_pdf")
 ]
