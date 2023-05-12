0e5ba5d1bca0c0df08cfb7ca69e02e8b7209aeb2
andrew <andrewrfofficial@gmail.com>
2023-05-12 18:18:25 +0200

    beta v-0.1.26

--- compras/models.py
+++ compras/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Proveedor(models.Model):
@@ -9,6 +10,7 @@ class Proveedor(models.Model):
 
 class OrdenDeCompra(models.Model):
     numero_orden= models.AutoField(primary_key=True)
+    fecha_orden = models.DateField( default=timezone.now)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, to_field="id")
     archivo = models.FileField(null=True, upload_to='documents/ordenescompra')
     monto = models.BigIntegerField()
--- compras/views.py
+++ compras/views.py
@@ -1,46 +1,81 @@
 from django.shortcuts import render, HttpResponse
 from django.template.loader import get_template
 from .models import OrdenDeCompra, Proveedor
+from io import BytesIO
+from weasyprint import HTML
+from datetime import datetime
 
 # Create your views here.
 def panel(request):
-    return render(request,"panel-compras.html")
+    return render(request, "panel-compras.html")
+
 
 def orden_compras(request):
-    if request.method== "GET":
+    if request.method == "GET":
         proveedores = Proveedor.objects.all()
-        return render(request,"ordencompra.html", {"proveedores": proveedores})
+        return render(request, "ordencompra.html", {"proveedores": proveedores})
     else:
+        datos = []
+        nombres = request.POST.getlist("nombre_producto")
+        print(request.POST)
         precios = request.POST.getlist("precio")
         cantidades = request.POST.getlist("cantidad")
-        contador = 0
-        print(request.POST)
+        subtotal_productos = 0
 
+        contador = 0
 
         for precio in precios:
-            monto_gasto = precio * cantidades[contador]
-            monto_restante = monto_gasto + request.POST["presupuesto"]
-            contador +=1
+            subtotal_producto = int(precio) * int(cantidades[contador])
+            subtotal_productos += subtotal_producto
+            datos.append(
+                {
+                    "producto": nombres[contador],
+                    "precio": precio,
+                    "cantidad": cantidades[contador],
+                    "subtotal_producto": subtotal_producto,
+                }
+            )
 
+            contador += 1
+
+        total = int(request.POST["presupuesto"]) - subtotal_productos
         template = get_template("pdf_ordendecompra.html")
-        html = template.render({"monto_restante":monto_restante, })
 
-        proveedores = Proveedor.objects.all()       
-        return render(request,"ordencompra.html", {"proveedores": proveedores})
-    
+
+        nueva_orden_compra = OrdenDeCompra.objects.create(
+            proveedor = request.POST["proveedor_id"],
+            monto = total
+        )
+        nueva_orden_compra.save()
+
+
+        html = template.render(
+            {"presupuesto": request.POST["presupuesto"], "datos": datos, "total": total, "orden":nueva_orden_compra}
+        )
+        pdf_file = HTML(string=html).write_pdf()
+        file_object = BytesIO(pdf_file)
+
+        nombre = 'OrdenCompra-{}.pdf'.format(datetime.now())
+
+        nueva_orden_compra.archivo.save(nombre ,file_object)
+
+        proveedores = Proveedor.objects.all()
+        return render(request, "ordencompra.html", {"proveedores": proveedores})
+
+
 def añadir_proveedor(request):
-    if request.method== "GET":
-        return render(request,"añadir_proveedor.html")
+    if request.method == "GET":
+        return render(request, "añadir_proveedor.html")
     else:
         try:
             nuevo_proveedor = Proveedor.objects.create(
-                nombre = request.POST["nombre"],
-                direccion = request.POST["direccion"],
-                numero_telefono = request.POST["telefono"]
+                nombre=request.POST["nombre"],
+                direccion=request.POST["direccion"],
+                numero_telefono=request.POST["telefono"],
             )
             nuevo_proveedor.save()
             status = "Proveedor Creado Satisfactoriamente"
-            return render(request,"añadir_proveedor.html", {"status":status})
+            return render(request, "añadir_proveedor.html", {"status": status})
         except:
             status = "Ha ocurrido un error"
-            return render(request,"añadir_proveedor.html", {"status":status})
+            return render(request, "añadir_proveedor.html", {"status": status})
