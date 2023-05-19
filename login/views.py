0b6ce6ee7c5ad3963d85360dc6311ce681b353a4
andrew <andrewrfofficial@gmail.com>
2023-05-19 17:30:22 +0200

    beta v-0.1.34

--- login/views.py
+++ login/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+from compras.models import OrdenDeCompra, Proveedor
+from items.models import Producto
+from ventas.models import Cliente, Factura
+
 
 def login_sesion(request):
     if request.method == "GET":
@@ -13,24 +17,46 @@ def login_sesion(request):
             password=request.POST["password"],
         )
         if user == None:
-            return render(request, "login_form.html", {"error": "Ha ocurrido un error, por favor inténtelo nuevamente"})
+            return render(
+                request,
+                "login_form.html",
+                {"error": "Ha ocurrido un error, por favor inténtelo nuevamente"},
+            )
         else:
             login(request, user)
-            return redirect('dashboard')
+            return redirect("dashboard")
 
 
 def inicio(request):
     return render(request, "inicio.html")
 
+
 @login_required
 def dashboard(request):
-    print(request.session)
-    return render(request, "dashboard.html")
+    proveedores = Proveedor.objects.all()
+    clientes = Cliente.objects.all()
+    ordenes_de_compra = OrdenDeCompra.objects.all()
+    productos = Producto.objects.all()
+    facturas = Factura.objects.all()
+
+    return render(
+        request,
+        "dashboard.html",
+        {
+            "proveedores": proveedores,
+            "clientes": clientes,
+            "ordenesdecompra": ordenes_de_compra,
+            "productos":productos,
+            "facturas":facturas
+        },
+    )
+
 
 @login_required
 def logout_sesion(request):
     logout(request)
-    return redirect('home')
+    return redirect("home")
+
 
 def LoginPerso(LoginView):
-    template_name="login_form.html"
+    template_name = "login_form.html"
